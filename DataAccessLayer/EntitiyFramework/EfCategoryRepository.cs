6e82c86d5a457f67462cc6f056961008ba89aa19
RamazanBerkan <78075121+RamazanBerkan@users.noreply.github.com>
2021-12-11 11:58:50 +0300

    Add project files.

---
+++ DataAccessLayer/EntitiyFramework/EfCategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Repositories;
+using EnitityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntitiyFramework
+{
+    public class EfCategoryRepository:GenericRepository<Category>, ICategoryDal
+    {
+    }
+}
---
+++ EnitityLayer/Concrete/About.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnitityLayer.Concrete
+{
+   public class About
+    {
+        [Key]
+
+        public int AboutId { get; set; }
+
+        public string AboutDetalis1 { get; set; }
+        public string AboutDetalis2 { get; set; }
+
+        public string AboutIamge1 { get; set; }
+        public string AboutIamge2 { get; set; }
+
+        public string AboutMapLocation { get; set; }
+        public bool AboutStatus { get; set; }
+
+    }
+}
